b9b26cbe3a099cbd364ecbc4d3a1e238874ce42a
Marcos-psr <110942404+Marcos-psr@users.noreply.github.com>
2023-07-27 04:10:00 -0300

    inserindo scroll

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,29 @@
-let btn = document.getElementById("ham-btn");
-let menuSpace = document.getElementById("menu-space");
-let menuItems = document.getElementById("menu-items");
+const btn = document.getElementById("ham-btn");
+const menuSpace = document.getElementById("menu-space");
+const menuItems = document.getElementById("menu-items");
+
+document.documentElement.className += " js";
 
 btn.addEventListener("click", function () {
   menuItems.classList.toggle("menu__active");
-  menuSpace.classList.toggle("activate-space")
+  menuSpace.classList.toggle("activate-space");
 });
+
+const sections = document.querySelectorAll(".scroll-js");
+function ativaSections() {
+  sections.forEach((section) => {
+    const sectionTop =
+      section.getBoundingClientRect().top - window.innerHeight * 0.6;
+
+    if (sectionTop < 0) {
+      section.classList.add("ativo");
+      const divisoria = section.nextElementSibling;
+      if (divisoria) {
+        divisoria.classList.add("ativo");
+      }
+    }
+  });
+}
+ativaSections();
+
+window.addEventListener("scroll", ativaSections);
